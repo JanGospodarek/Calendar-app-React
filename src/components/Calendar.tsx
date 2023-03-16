9a525b58220e9ebf5710d5a189cb148bf927ef61
JanGospodarek <jan.gospo@onet.pl>
2023-03-16 19:41:46 +0100

    basic lay

---
+++ src/components/Calendar.tsx
@@ -0,0 +1,13 @@
+import { Card, Stack, Typography } from "@mui/material";
+
+export function Calendar() {
+  return (
+    <Card sx={{ padding: "10px" }}>
+      <Stack>
+        <Typography variant="h5" color="initial">
+          Calendar
+        </Typography>
+      </Stack>
+    </Card>
+  );
+}
---
+++ src/components/Events.tsx
@@ -0,0 +1,13 @@
+import { Card, Stack, Typography } from "@mui/material";
+
+export function Events() {
+  return (
+    <Card sx={{ padding: "10px" }}>
+      <Stack>
+        <Typography variant="h5" color="initial">
+          Events
+        </Typography>
+      </Stack>
+    </Card>
+  );
+}
